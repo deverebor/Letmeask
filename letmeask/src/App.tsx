353097ffee0c60197be2c46ccfe3378254374f1f
Lucas Souza <lucasp.sdev@gmail.com>
2021-06-21 00:11:35 -0300

    Aula 01 - Configurações do Firebase

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+
+    </div>
+  );
+}
+
+export default App;
