4224504bb0cab2b1f9a1bacfd4a3bc1229a5bbd6
Lucas Souza <lucasp.sdev@gmail.com>
2021-06-22 01:50:46 -0300

    Aula 02 - Autenticaçã

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,16 +1,20 @@
-import { Button } from './components/Button';
+//Importação dos componentes
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom' 
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <div>
-
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-
-    </div>
+    <BrowserRouter>
+     <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewRoom}/>
+        </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ letmeask/src/contexts/AuthContext.tsx
@@ -0,0 +1,76 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+import { auth, firebase } from "../services/firebase"
+
+//Criação das tipagens utilizadas. 
+type User = {
+    id: string;
+    name: string;
+    avatar: string;
+  }
+  
+  type AuthContextType = {
+    user: User | undefined; //No primeiro momento não existe usuário logado então é undefined.
+    signInWithGoogle: () => Promise<void>
+  }
+  
+  type AuthContextProviderProps = {
+      children: ReactNode;
+  }
+  
+
+export const AuthContext = createContext({} as AuthContextType)//Aqui se passa o formato da informação que vai armazenar dentro do contexto, nesse caso um objeto
+
+export function AuthContextProvider(props: AuthContextProviderProps){
+    const [user, setUser] = useState<User>()
+
+  useEffect(() => {
+    //Event listen. Se for detectado que um usuário já foi logado na aplicação vai ser retornado o usuário.
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if(user) {
+          const { displayName, photoURL, uid } = user
+  
+          if (!displayName || !photoURL) {
+            throw new Error('Missing Information from Google Account.')
+          }
+  
+          setUser({
+            id: uid, 
+            name: displayName,
+            avatar: photoURL
+          })
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [] )
+
+  async function signInWithGoogle() {
+    //Autenticação do usuário
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    const result = await auth.signInWithPopup(provider)
+
+      if(result.user) {
+        const { displayName, photoURL, uid } = result.user
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing Information from Google Account.')
+        }
+
+        setUser({
+          id: uid, 
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+
+
+  }
+    return(
+        <AuthContext.Provider value={{ user, signInWithGoogle }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
