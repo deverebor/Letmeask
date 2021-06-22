1533f4b002b480b212ce26fe59d8102c222d75cb
Lucas Souza <lucasp.sdev@gmail.com>
2021-06-22 02:01:32 -0300

    Aula 02 - Autenticação
    Criei a parte de autenticação do usuário no reactjs integrando com o firebase.

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -5,7 +5,7 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
 import {Button} from '../components/Button'
-import { useAuth } from '../hooks/useAuth'
+//import { useAuth } from '../hooks/useAuth'
 
 import '../styles/auth.scss'
 
